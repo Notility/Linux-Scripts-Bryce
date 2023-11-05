5ef0213bf6f4d717956905b68ad666d9996ede3e
Notility <92754833+Notility@users.noreply.github.com>
2023-11-05 12:18:22 -0600

    ICMP requests from internet network enabled

--- iptablesPortBashForSPLUNK.sh
+++ iptablesPortBashForSPLUNK.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ####YOU MUST RUN THIS AS SUDO
 
+echo "Please enter what team you are currently on"
+read team number
 
 iptables -F
 #Flush All previous iptables rules
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT DROP
+iptables -P INPUT -i lo -j ACCEPT
 
 #Adds Splunk Ports
 iptables -A INPUT -p tcp --dport 8000 -j ACCEPT
@@ -14,9 +14,16 @@ iptables -A INPUT -p tcp --dport 8065 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8191 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8865 -j ACCEPT
 
+# Allow ICMP requests from internal network
+iptables -A INPUT -p icmp -s 172.20.240.10/24 -j ACCEPT
+iptables -A INPUT -p icmp -s 172.20.241.0/24 -j ACCEPT
+iptables -A INPUT -p icmp -s 172.20.242.0/25 -j ACCEPT
+
 #removes ssh for good measure >:D
 iptables -A INPUT -p tcp --dport 22 -j DROP
 
+#Drop all other incomming traffic
+iptables -A INPUT -j DROP
 # To delete any rule list otu the current rules by line number with:
 #   iptables -L --line-numbers
 
@@ -27,3 +34,6 @@ iptables -A INPUT -p tcp --dport 22 -j DROP
 iptables-save > /etc/sysconfig/iptables
 service iptables save
 service iptables restart
+
+#Enable on startup
+chkconfig iptables on
